47a023d02c1885cb815dd9e9c1bcb07103c8e57d
vikramforsk2019 <vsg16492cse2016@gmail.com>
2019-07-22 08:26:29 +0530

    suicide_case

---
+++ [Forsk] _Project .py
@@ -0,0 +1,110 @@
+"""
+Project 01:
+
+This Suicide-Rate dataset has 27820 number of observations (rows) and 12
+features (columns). One thing that is obvious is the HDI for year feature
+has a large number of missing value and hence it wouldn't be considered
+later on.
+
+Description of features:
+
+1. country: country name
+
+101 unique value
+
+2. year: years
+
+ranged between 1985 - 2016
+
+3. sex: gender
+
+female or male
+
+4. age: age groups
+
+5-14 years
+
+15-24 years
+
+25-34 years
+
+35-54 years
+
+55-74 years
+
+75+ years
+
+5. suicides_no: number of suicide cases in the given country, year, sex and
+age group
+
+6. population: number of population
+
+7. suicides/100k pop: suicide rate normalized by population number
+
+8. country-year: country name + year
+
+9. HDI for year: The Human Development Index (HDI) is a statistical
+composite index of life expectancy, education, and per capita income
+indicators, which are used to rank countries into four tiers of human
+development.
+
+10. gdp_for_year: Gross Domestic Product (GDP)
+
+11. gdp_per_capita: GDP per capita is a measure of a country's economic
+output that accounts for its number of people. It divides the country's
+gross domestic product by its total population.
+
+12. generation: generation group
+
+Generation X
+
+Silent
+
+G.I. Generation
+
+Boomers
+
+Millennials
+
+Generation Z
+
+Find out the following:
+
+
+
+1. visualize the  number of suicides of both male and females of different
+age group for every year from 1986-2016
+
+   & find which age group has the highest number of suside rate.
+
+2. Which country has the highest number of Suside rate visualize it.
+
+3. Which country has highest & Lowest suside rate in 2006 visualize it.
+
+4. apply all classification algorithm and find best results,check the score
+and accuracy.then predict for this data
+
+   country ----> Albania
+
+   year ----> 2014
+
+   Sex ----> Male
+
+   Age ----> 24
+
+   Suside no. ----> 22
+
+   population ----> 279800
+
+   country-year ----> 7.11
+
+   gdp_for_year ($)  ---->2,10,56,21,800
+
+   gdp_per_capita ($)  ---->749
+
+   according to data find it was lie on which generation.
+
+5. Which country has the lowest GDP rate in 2014. visualize it.
+
+Dataset: Collect from Forsk team.
+"""
